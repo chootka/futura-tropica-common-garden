b5c86c63c574a1dbc50a383bf7e156060f9f0d67
chootka <com@chootka.com>
2021-02-14 12:11:18 +0100

    tracing iframe

--- public/js/showdata.js
+++ public/js/showdata.js
@@ -105,7 +105,8 @@ $.getJSON(showdata, function( json ) {
             slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
                 var data = { "event":"command", "func":"pauseVideo", "args":"" };
                 var message = JSON.stringify(data);
-                console.log("beforeChange", slick.$slides);
+                console.log("currentSlide", slick.$slides[currentSlide]);
+                console.log("iframe", $("iframe", slick.$slides[currentSlide])[0]);
                 $("iframe", slick.$slides[currentSlide])[0].contentWindow.postMessage(message, '*');
             });
 
