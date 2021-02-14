c4cab5d4237dc46b528b452c2470e2b63c6a8273
chootka <com@chootka.com>
2021-02-14 14:03:12 +0100

    fixed homebutton link

--- public/js/popUp.js
+++ public/js/popUp.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', function(e) {
 if (window.location.search.includes("username")) {
     const urlParams = new URLSearchParams(window.location.search);
     document.body.querySelector(".popUp .username").value = urlParams.get("username");
-    document.body.querySelector(".homeButton a").href = urlParams.get("username");
+    document.body.querySelector(".homeButton a").href = "/?username=" + urlParams.get("username");
 }
 
 if (window.location.search.includes("preview")) {
@@ -43,7 +43,7 @@ function closePopUp(event) {
 
     // update username
     username = document.body.querySelector(".popUp .username").value;
-    document.body.querySelector(".homeButton a").href = username;
+    document.body.querySelector(".homeButton a").href = "/?username=" + username;
     socket.emit("setUsername", username);
 
     console.log("username", username);
