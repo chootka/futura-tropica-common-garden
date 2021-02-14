16ffda9662be83c355258d30fe842aaad8d470b7
chootka <com@chootka.com>
2021-02-14 11:00:04 +0100

    adding data src back into video els

--- public/js/showdata.js
+++ public/js/showdata.js
@@ -66,8 +66,10 @@ $.getJSON(showdata, function( json ) {
         } else if ( json.works[ i ].localVideo ) {
             article.append( '<video src="' + json.works[i].src + '" autoplay muted class="iframe"></video>' );
         } else if ( json.works[ i ].youtube ) {
+            article.attr("data-src", json.works[ i ].url)
             article.append( '<div class="iframe"><iframe id="iframe' + (i+1) + '" class="iframe" data-showVolume="true" data-volume="100" scrolling="no" frameborder="0" allow="autoplay" muted width="' + json.works[ i ].width + '" height="' + json.works[ i ].height + '"></iframe></div>' );
         } else if ( json.works[ i ].vimeo ) {
+            article.attr("data-src", json.works[ i ].url)
             article.append( '<div class="iframe"><iframe id="iframe' + (i+1) + '" class="iframe" data-showVolume="true" data-volume="1" scrolling="no" frameborder="0" allow="autoplay" muted width="' + json.works[ i ].width + '" height="' + json.works[ i ].height + '"></iframe></div>' );
         } 
         if (json.works[i].youtubeSync) {
