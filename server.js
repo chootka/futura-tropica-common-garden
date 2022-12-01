16d2349cfcf4b26b86f6fe9855e852f11f922687
Sarah Grant <com@chootka.com>
2022-12-01 16:57:31 +0000

    update to kinshasa assets

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ let maxRoomSize = 200;
 
 let slideshows = [];
 
-let subdomain = "public";
+let subdomain = "colombia-peer";
     // subdomain = "join";
     // subdomain = "bogota";
     // subdomain = "kinshasa";
